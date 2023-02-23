d18a4716f6f3cfd249b79df27b9c170c3ad5eda9
Vitor Davi Pereira Soares <79873418+vitordavips@users.noreply.github.com>
2023-02-23 17:37:53 -0400

    fazendo o teste com a api localStore

--- public/gráfico/grafico.js
+++ public/gráfico/grafico.js
@@ -1,23 +1,26 @@
+const numCliques = localStorage.getItem('numCliques') || 0;
+const bomCliques = localStorage.getItem('bomCliques') || 0;
+const regularCliques = localStorage.getItem('regularCliques') || 0;
+
 const ctx = document.getElementById('grafico');
 
-// Gráfico
 new Chart(ctx, {
     type: 'bar',
     data: {
         labels: ['Ótimo', 'Bom', 'Ruim'],
         datasets: [{
             label: 'Ótimo',
-            data:[31, 0, 0],
+            data:[numCliques],
             backgroundColor: 'rgb(0,128,128)',
             borderWidth: 2 
         },{
             label: 'Bom',
-            data:[0, 55, 00],
+            data:[0, bomCliques, 0],
             backgroundColor: 'rgb(0,0,205)',
             borderWidth: 2 
         },{
             label: 'Ruim',
-            data:[0, 0, 23],
+            data:[0, 0, regularCliques],
             backgroundColor: 'rgb(255,0,0)',
             borderWidth: 2 
         }]
---
+++ public/main.js
@@ -0,0 +1,34 @@
+//const socket = io();
+
+const otimo = document.getElementById("otimo");
+const bom = document.getElementById("bom");
+const regular = document.getElementById("regular");
+let numCliques = localStorage.getItem('numCliques') || 0;
+let bomCliques = localStorage.getItem('bomCliques') || 0;
+let regularCliques = localStorage.getItem('regularCliques') || 0;
+
+/* Esse código verifica se já existe um valor armazenado para 'numCliques' na 'localStorage'.
+Se não houver, ele define o valor inicial como '0'.Em seguida, ele adiciona um evento de clique ao botão
+que atualizar o valor de 'numCliques' e o armazena na 'localStorage'*/
+otimo.addEventListener('click', () => {
+    numCliques++;
+    localStorage.setItem('numCliques', numCliques);
+});
+
+bom.addEventListener('click', () => {
+    bomCliques++;
+    localStorage.setItem('bomCliques', bomCliques);
+});
+
+regular.addEventListener('click', () => {
+    regularCliques++;
+    localStorage.setItem('regularCliques', regularCliques);
+});
+
+
+
+
+/*const urlSearch = new URLSearchParams(window.location.search);
+const click = urlSearch.get("click")
+
+console.log(click)*/
